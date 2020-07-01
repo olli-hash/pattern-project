6e517a65a1fd1e269441be79bad04a25e2b98cfd
olli-hash <52583865+olli-hash@users.noreply.github.com>
2020-07-01 12:07:25 +0200

    m

---
+++ js/ui_events.js
@@ -0,0 +1,33 @@
+
+function call_ui() {
+    // l($('form').length)
+    var laststate = true
+    $('form').on("click", function() {
+        var ui_ex3_state1 = {
+            transition : "2s" ,
+            width : "500px",
+            backgroundColor : "#faa"
+        }
+        var ui_ex3_state2 = {
+            transition : "2s" ,
+            width : "500px",
+            backgroundColor : "#faa"
+        }
+        $(this).css(ui_ex3_state1)
+    })
+
+
+    $('p').on("click", function() {
+
+        if ($(this).hasClass("select1")) {
+            $(this).removeClass("select1")
+        } else {
+            $(this).addClass("select1")
+        }
+
+    })
+
+
+
+
+}
