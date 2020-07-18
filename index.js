e461bac01b4dbd50a98c4c6e5a542e56e2842711
olli-hash <52583865+olli-hash@users.noreply.github.com>
2020-07-18 13:21:35 +0200

    m

--- index.js
+++ index.js
@@ -3,18 +3,31 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const path = require('path')
 var app = express()
-// var cookieParser = require('cookie-parser');
-// app.use(cookieParser())
-// var bodyParser = require('body-parser');
 
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
 app.set('views', path.join(__dirname, 'views'))
 
 // app.use(express.static("served"))
-app.use("/script",express.static("script")) 
+app.use("/js",express.static("js"))
 
-app.use("/css",express.static("css")) 
+app.use("/jquery-handlebars",express.static("jquery-handlebars"))
+
+app.use("/css",express.static("css"))
+
+app.get("/basic_index.html", function(req, res){
+    res.sendFile("basic_index.html", {root: path.join(__dirname, '.')})
+    // console.log(path.join(__dirname, '.'))
+})
+
+app.get("/ajax_getsomething", function(req, res){
+
+    // res.send("hi")
+
+    res.send({   friend: "Oliver", age:40          })
+
+})
+
 
 
 app.get("/write", function(req, res){
@@ -26,3 +39,19 @@ app.get("/write", function(req, res){
 app.listen(port, function(){
     console.log(`http://localhost:${port}/write         `)
 })
+
+
+/*
+
+Es gibt 4 Wege Content zu generieren und zu senden:
+
+1. literal String   ...   res.send("___")
+
+2. literal Objects  ...   res.send({____})
+
+3. gerenderter String  ... res.render(____)
+
+4. selfmade JS-Function-Rendering, on-the-fly oder in großem Stil
+
+
+*/
--- js/ui_events.js
+++ js/ui_events.js
@@ -52,7 +52,28 @@ function call_ui() {
         $(this).parent().children(".code1").slideDown()
         $(this).parent().children(".code1_output").slideDown()
         $(this).parent().children(".example_contextobject").slideDown()
-})
+    })
+
+
+
+    $("#ajax1").click(function(e){
+        e.stopPropagation()
+        $.ajax({
+            url: "http://rest-service.guides.spring.io/greeting"
+        }).then(function(data) {
+           $('#ajax1').append(data.id);
+           $('#ajax1').append(data.content);
+        })
+
+    })
+    $("#ajax2").click(function(e){
+        e.stopPropagation()
+    })
+    $("#ajax3").click(function(e){
+        e.stopPropagation()
+
+    })
+
 
 
 
